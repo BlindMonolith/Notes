bae08eb94fdd7ae174aab75db16067920c9cdde1
BlindMonolith <109252073+BlindMonolith@users.noreply.github.com>
2022-07-21 20:56:52 -0500

    Update test_version1.py
    Still working on this, however, I decided to redesign the code structure along with having to push out ODM upgrades so i wasnt able to do a ton today.

--- code/DynamicInventory/test_version1.py
+++ code/DynamicInventory/test_version1.py
@@ -1,51 +1,63 @@
-'''
+import os
 
-Author : Sam McCoy
-
-Description : Calls the ServiceNow API to get a list of servers
-
-Imports: [
-
-requests : Used to call the REST API and to GET from ServiceNow, passed in the url, username, and password.
-
-sys : Used for error handling through redirecting error files and to replace the python print() functunality with a sys.stdout.write() which can be more reliable.
-
-os : Allows for file handling and a number of other useful functionalities.
-
-json : Allows for the parsing of the requests output into a json file so that it can be turned given to dynamic inventory to parse and prune.
-
-]
-
-'''
-
- 
+import certifi
 
 import requests
+
 import sys
-import os
+
+import time
+
 import json
 
-class callerAPI():
-    def __init__(self):
-        print(requests.certs.where())
-        self.LOG_STATUS = "status_logfile"
-        self.LOG_JSON = "jsonTest.json"
-        if os.path.exists(self.LOG_STATUS):
-            self.API_status_code = open(self.LOG_STATUS,"a")
+
+
+
+class InventoryModule():
+
+    NAME = 'bts.servicenow.cmdb_servers'
+
+    def verify_file(self,path):
+
+        pass
+
+    def get_table(self,table,display_value = True):
+
+       
+
+        '''
+
+        # UNCOMMENT THESE THREE LINES BEFORE DEPLOYING TO SERVER
+
+        host = os.getenv("SN_HOST")
+
+        username = os.getenv("SN_USERNAME")
+
+        password = os.getenv("SN_PASSWORD")
+
+        '''  
+
+       
+
+        url = "{}/api/now/table/{}".format(host,table)
+
+        params = {'sysparm_display_value': display_value, 'sysparm_exclude_reference_link': 'true'}
+
+       
+
+   
+
+        request= requests.get(url=url , params= params, auth= (username,password), verify= 'Inventory\\perm_chain.pem')
+
+        if request.status_code == 200:
+
+            parsed_dict = json.loads(request.json())
+
+            return parsed_dict['result']
+
         else:
-            self.API_status_code = open(self.LOG_STATUS,"w")
-        if os.path.exists(self.LOG_JSON):
-            os.remove(self.LOG_JSON)
-            self.parsedJSON = open(self.LOG_JSON,"w")
-        else:
-            self.parsedJSON = open(self.LOG_JSON,"w")
-        self.restAPIcall = requests.get(url,auth=(username,password))
-        self.status_code = self.restAPIcall.status_code
-        if (self.status_code == 200):
-            self.API_status_code.write('SUCCESS;200\n')
-            self.parsed_dict = json.loads(self.restAPIcall.text)
-            self.parsed_json = json.dumps(self.parsed_dict,indent= 4)
-            self.parsedJSON.write(self.parsed_json)
-        else:
-            self.API_status_code.write('FAILURE:'+str(self.status_code)+"\n")
-            sys.stdout.write("failure")
+
+            return dict({None})
+try1 = InventoryModule()
+
+try1.get_table('cmdb_ci_app_server_websphere')
